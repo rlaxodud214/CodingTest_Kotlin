8de37a855564811cdda6e6815382c4835274b499
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2023-09-09 07:00:37 +0900

    [unrated] Title: 바탕화면 정리, Time: 6.46 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.kt
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.kt
@@ -1,22 +1,31 @@
+import kotlin.math.*
+
 class Solution {
     fun solution(wallpaper: Array<String>): IntArray {
         var answer = mutableListOf(wallpaper.size, wallpaper[0].length, 0, 0)
         
-        for((x, wall) in wallpaper.withIndex()) {
-            for((y, char) in wall.withIndex()) {
-                if(char == '.')
+        for(x in wallpaper.indices) {
+            for(y in wallpaper[x].indices) {
+                if(wallpaper[x][y] == '.')
                     continue
                 
-                if(answer[0] > x)
-                    answer[0] = x
-                if(answer[1] > y)
-                    answer[1] = y
-                if(answer[2] <= x)
-                    answer[2] = x+1
-                if(answer[3] <= y)
-                    answer[3] = y+1
+                // if(answer[0] > x)
+                //     answer[0] = x
+                // if(answer[1] > y)
+                //     answer[1] = y
+                // if(answer[2] <= x)
+                //     answer[2] = x+1
+                // if(answer[3] <= y)
+                //     answer[3] = y+1
+                
+                answer[0] = min(x, answer[0])
+                answer[1] = min(y, answer[1])
+                answer[2] = max(x, answer[2])
+                answer[3] = max(y, answer[3])
             }
         }
+        answer[2]++
+        answer[3]++
         return answer.toIntArray()
     }
 }
