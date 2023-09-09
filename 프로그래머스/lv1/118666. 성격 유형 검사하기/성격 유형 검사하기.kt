d155bdd13368b428cb6b46ac5eeb104bd60f88f5
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2023-09-09 12:24:51 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 32.54 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
@@ -4,28 +4,21 @@ class Solution {
         // 1. survey_result를 저장할 맵 선언 / choices
         var survey_R = mutableMapOf<String, Int>("RT" to 0, "CF" to 0, "JM" to 0, "AN" to 0)
         
-        // 2. choices -> score : 0 -> 3, 1 -> 3, 2 -> 1,  3 -> 0, 4 -> -1 / score == 3 - choices
+        // 2. choices -> score : 1 -> 3, 4 -> 0, 5 -> -1 / score == 4 - choices
         for((index, type) in survey.withIndex()) {
             if(choices[index] == 4) continue
             
-            println("$type, ${choices[index]}")
             if(survey_R.containsKey(type)) {
-                survey_R[type] = survey_R[type]!! + 4 - choices[index]
+                survey_R[type] = survey_R[type]!! + (4 - choices[index])
             } else {
-                var typeArray = type.toCharArray()
-                typeArray.reverse()
-                val reverseType = String(typeArray)
-                survey_R[reverseType] = survey_R[reverseType]!! + (choices[index] - 4)
-                println("$type -> $reverseType")
+                survey_R[type.reversed()] = survey_R[type.reversed()]!! - (4 - choices[index])
             }
-            println("$survey_R")
         }
 
         // 3. score >= 0이면 앞의 유형 else 뒤의 유형
         for ((key, score) in survey_R) {
             answer += if(score >= 0) key[0] else key[1]
         }
-        println(survey_R)
         return answer
     }
 }
