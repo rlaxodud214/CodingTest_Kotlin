35cf0567c75228deb01646fe7bcc00cad3bb0bb9
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2023-09-09 10:14:51 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 15.65 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
@@ -3,9 +3,9 @@ class Solution {
         var answer: IntArray = intArrayOf()
         var termsValues = mutableMapOf<String, Int>()
         var privaciesValues = mutableListOf<Int>()
+
         // 1. 2000.01.01 날짜를 기준으로 today, terms, privacies의 날짜 형식을 숫자로 변경한다.
-        val (today_year, today_month, today_day) = today.split(".").map{ it.toInt() }
-        val todayValue = (today_year-2000) * 12 * 28 + (today_month-1) * 28 + today_day-1
+        val todayValue = dateParser(today)
         
         terms.forEach {
             termsValues[it.split(" ")[0]] = it.split(" ")[1].toInt() * 28
@@ -13,13 +13,20 @@ class Solution {
         
         privacies.forEach {
             val type:String = it.split(" ")[1]
-            val (y, m, d) = it.split(" ")[0].split(".").map{ it.toInt() }
+            var privaciesValue = dateParser(it.split(" ")[0])
             // 날짜형식을 숫자로 변경하면서 type의 유효기간만큼 더하고 오늘 날짜를 뺀다.
-            val privaciesValue = (y-2000) * 12 * 28 + (m-1) * 28 + d-1 + termsValues[type]!! - todayValue
-            privaciesValues.add(privaciesValue)
+            privaciesValues.add(privaciesValue + termsValues[type]!! - todayValue)
         }
         // privaciesValues의 value가 0보다 작거나 같다면 유효기간 만료를 뜻한다.
         answer = privaciesValues.indices.filter { privaciesValues[it] <= 0 }.map {it + 1}.toIntArray()
         return answer
     }
+    // '다른 사람의 풀이 아이디어'
+    // 2차례 사용할 dateParser 코드를 함수로 구현해서 코드 중복 제거
+    /** 현재 날짜 정보를 Int형으로 바꿔주는 함수 */
+    fun dateParser(info: String): Int {
+        val (year, month, day) = info.split(".").map {it.toInt()}
+        return (year - 2000) * (28*12) + month * 28 + day
+    }
+    // day 기준 int 변환이 아닌 month를 기준으로 변환하여 28진수 비교를 하는 코드도 있었음
 }
