57917c56c97b57ffb1649420a8cba16ae89bbe59
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2023-10-14 21:33:39 +0900

    [level 1] Title: 공원 산책, Time: 10.13 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.kt
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.kt
@@ -3,74 +3,69 @@ class Solution {
         // 0. 공원의 최대 인덱스 구하기
         var (H, W) = intArrayOf(park.size, park[0].length)
         
-        // 1. park에서 S 및 X들의 위치(좌표x-y)를 찾는다.
+        // 1. park에서 S(출발점) 좌표 찾기
         var point = intArrayOf(-1, -1)
-        var x = mutableListOf<String>()
         park.forEachIndexed { i, row ->
-            row.mapIndexedNotNull { j, char ->
-                if (char == 'S') {
+            row.forEachIndexed { j, char ->
+                if (char == 'S')
                     point = intArrayOf(i, j) 
-                }
-                if (char == 'X') {
-                    x.add("$i, $j") // 뒤에서 in으로 처리하기 위해 String으로 변경
-                }
             }
         }
                 
         // 2. routes를 순회하며 A), B) 인 경우 return -> 아니면 C) 수행
-        println("처음 위치 : ${point.contentToString()}")
+        // println("처음 위치 : ${point.contentToString()}")
         routes.forEach { route ->
-            var op:String = route.split(" ")[0]
-            var n:Int = route.split(" ")[1].toInt()
-            var newPoint = point
-            var check = mutableListOf<String>()
- 
-            // A) 범위 벗어나면 return
-            // B) 현재 좌표부터 route 적용된 좌표까지 인덱스를 routeArray 배열로 뽑는다.
-            //    check와 x 간에 중복이 있으면 return
-            // C) point 업데이트하기
+            val (op, nStr) = route.split(" ")
+            val n = nStr.toInt() ?: 0
+            val (x, y) = point
+                        
             when (op) {
                 "N" -> {
-                    if(point[0] - n < 0)
+                    // A) 범위 벗어나면 return
+                    if(x - n < 0)
                         return@forEach
-                    
+
+                    // B) 현재 좌표부터 route 적용된 좌표까지 순회하며 X와 중복이 있으면 return
                     for (i in 1..n)
-                        if("${point[0] - i}, ${point[1]}" in x) 
+                        if(park[x - i][y] == 'X') 
                             return@forEach
-                    newPoint[0] = point[0] - n
+                    
+                    // C) point 업데이트하기
+                    point[0] = x - n
                 }
                 "S" -> {
-                    if(point[0] + n > H - 1)
+                    if(x + n >= H )
                         return@forEach
                     
                     for (i in 1..n)
-                        if("${point[0] + i}, ${point[1]}" in x) 
+                        if(park[x + i][y] == 'X') 
                             return@forEach
-                    newPoint[0] = point[0] + n
+                    
+                    point[0] = x + n
                 }
                 "W" -> {
-                    if(point[1] - n < 0)
+                    if(y - n < 0)
                         return@forEach
                     
                     for (i in 1..n)
-                        if("${point[0]}, ${point[1] - i}" in x) 
+                        if(park[x][y - i] == 'X') 
+
                             return@forEach
-                    newPoint[1] = point[1] - n
+                    
+                    point[1] = y - n
                 }
                 "E" -> {
-                    if(point[1] + n > W - 1)
+                    if(y + n >= W)
                         return@forEach
                     
                     for (i in 1..n)
-                        if("${point[0]}, ${point[1] + i}" in x) 
+                        if(park[x][y + i] == 'X')
                             return@forEach
-                    newPoint[1] = point[1] + n
+                    
+                    point[1] = y + n
                 }
             }
-            
-            // C) A, B가 아닌 경우 point 업데이트하기
-            point = newPoint
-            println("위치 이동 : ${point.contentToString()}")
+            // println("위치 이동 : ${point.contentToString()}")
         }
         return point
     }
