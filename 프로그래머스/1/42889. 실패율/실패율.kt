6d2f5b2aa23b9e51e1e4a2846e1c83f278ba40c3
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2023-10-14 16:13:46 +0900

    [level 1] Title: 실패율, Time: 11.75 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.kt
+++ 프로그래머스/1/42889. 실패율/실패율.kt
@@ -1,21 +1,19 @@
 class Solution {
     fun solution(N: Int, stages: IntArray): IntArray {
-        var failureRates = mutableListOf<Pair<Int, Float>>()
+        var rates = mutableListOf<Pair<Int, Float>>()
 
         // 1. N번 반복하면서 N회차 실패율을 failureRates에 넣기
         for (i in 1..N) {
-            // N회차 실패율 : 값이 N이하인 사용자의 수 / 값이 N이상인 사용자의 수
-            val failureRate = (stages.count { it == i }).toFloat() / (stages.count { it >= i }).toFloat()
-            failureRates.add(Pair(i, failureRate))
+            // N회차 실패율 = 값이 N인 사용자의 수 / 값이 N이상인 사용자의 수
+            val rate = (stages.count { it == i }).toFloat() / (stages.count { it >= i }).toFloat()
+            rates.add(Pair(i, rate))
         }
-        
-        print(failureRates)
-        /* 2. failureRates를 기반으로 answer 정렬하기
+        /* 2. rates 정렬
                 a. 실패율을 내림차순으로 정렬
                 b. 실패율이 같다면 작은 번호의 스테이지가 먼저 오게 
         */
-        failureRates = failureRates.sortedWith(compareBy({ -it.second }, { it.first })).toMutableList()
-        val temp = failureRates.map { pair -> pair.first }.toIntArray()
-        return temp 
+        
+        // [2. a+b] compareBy(첫 번째 기준, 두 번째 기준)으로 실패율이 같을 경우, 스테이지 번호를 기준으로 정렬
+        return rates.sortedWith(compareBy({ -it.second }, { it.first })).map { (stage, _) -> stage }.toIntArray()
     }
 }
