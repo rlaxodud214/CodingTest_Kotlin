3a63dc5b3918a479db0f9bd50c2f65527e55397a
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2023-10-18 01:14:36 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.46 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
@@ -1,6 +1,8 @@
 class Solution {
     fun solution(seoul: Array<String>): String {
         // 정렬한 뒤 탐색하는 것 보다는 바로 찾는 게 O(N)으로 가장 효율적일 것 같다.
-        return "김서방은 ${seoul.indexOf("Kim")}에 있다"
+        // [다른 사람의 풀이] indexOf 대신 indexOfFirst를 사용 -> 성능이 약 2배 좋음
+        // GPT Q&A : JVM과 JIT 컴파일러가 최신 버전이면 람다의 성능 차이일 수 있다.
+        return "김서방은 ${seoul.indexOfFirst { it == "Kim" }}에 있다"
     }
 }
