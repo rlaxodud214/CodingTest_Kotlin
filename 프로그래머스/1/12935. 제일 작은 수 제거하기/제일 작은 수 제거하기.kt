a8bec48ce06879e372c047f2465cefb93ecdeaf0
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2023-10-24 11:16:32 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 19.53 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
@@ -1,26 +1,30 @@
 class Solution {
     fun solution(arr: IntArray): IntArray {
-        var answer = intArrayOf()
-
-        if (arr.size == 1 && arr[0] == 10) {
+        if (arr.size == 1) {
             return intArrayOf(-1)
         }
-        val minValueIndex = arr.minValueOfIndex()
-        return arr.removeAt(minValueIndex)
-    }
 
+        val minValueIndex = arr.minValueOfIndex()
+        
+        val mutableList = arr.toMutableList()
+        mutableList.removeAt(minValueIndex)
+        return mutableList.toIntArray()
+    }
+    
     fun IntArray.minValueOfIndex(): Int {
         var minValueIndex = 0
+        
         this.forEachIndexed { index, value ->
             if (this[minValueIndex] > value)
                 minValueIndex = index
         }
+        
         return minValueIndex
     }
 
-    fun IntArray.removeAt(index: Int): IntArray {
-        return IntArray(this.size - 1) {
-            if (it < index) this[it] else this[it + 1]
-        }
-    }
+    // fun IntArray.removeAt(index: Int): IntArray {
+    //     return IntArray(this.size - 1) {
+    //         if (it < index) this[it] else this[it + 1]
+    //     }
+    // }
 }
