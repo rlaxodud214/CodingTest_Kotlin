e26d3b82eb7d67854cb3ddcf600442e49e846187
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-02-26 21:21:14 +0900

    [level 1] Title: 하샤드 수, Time: 5.89 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun solution(x: Int): Boolean {
-        var h = x.toString().map { it.toString().toInt() }.sumOf { it }
-        return true.takeIf { x % h == 0 } ?: false
+        var h = x.toString().map { it.toString().toInt() }.sum()
+        return if (x % h == 0) true else false
     }
 }
