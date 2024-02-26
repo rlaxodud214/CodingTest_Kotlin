7639cb83d86764d810aae31e49c75bf4767bbf45
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-02-26 21:18:15 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 28.51 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.kt
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.kt
@@ -1,5 +1,7 @@
 class Solution {
-    fun solution(n: Long): Long {
-        return n.toString().map {it}.sortedDescending().joinToString("").toLong()
-    }
+    fun solution(n: Long) = n.toString()
+        .map {it}
+        .sortedDescending()
+        .joinToString("")
+        .toLong()
 }
