67e7227212925486da2d6b1caa45f8fd32d5cd17
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-02-26 20:58:44 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.kt
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.kt
@@ -1,12 +1,11 @@
+import kotlin.math.*
+
 class Solution {
     fun solution(num: Int): String {
-        println(5 % 2 == 1)
-        
-        if(num == 0)
+        if(num == 0) {
             return "Even"
-        else if(num < 0)
-            return "Odd".takeIf{(num * -1) % 2 == 1} ?: "Even"
-        else
-            return "Odd".takeIf{num % 2 == 1} ?: "Even"
+        }
+        
+        return if(abs(num) % 2 == 1) "Odd" else "Even"
     }
 }
