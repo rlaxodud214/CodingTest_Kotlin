1b0de23b062d7c8e1deb9f55203238c2b8fe222c
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-02-29 12:39:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 20.08 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
@@ -1,16 +1,12 @@
 class Solution {
     fun solution(s: String): Int {
         var answer = s
-        
-        var match = mapOf( 
-            "zero" to "0", "one" to "1", "two" to "2", "three" to "3", "four" to "4", 
-            "five" to "5", "six" to "6", "seven" to "7", "eight" to "8", "nine" to "9"
-        )
-        
-        for ((key, num) in match) {
-            answer = answer.replace(key, num)
+        var match = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+        for ((num, text) in match.withIndex()) { // index == num
+            answer = answer.replace(text, num.toString())
         }
-            
+
         return answer.toInt()
     }
 }
