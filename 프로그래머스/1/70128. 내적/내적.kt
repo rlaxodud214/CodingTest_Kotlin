72aed3d08475083853de616b8e9243e142d6167c
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-02 17:47:12 +0900

    [level 1] Title: 내적, Time: 3.56 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.kt
+++ 프로그래머스/1/70128. 내적/내적.kt
@@ -1,8 +1,18 @@
 class Solution {
     fun solution(a: IntArray, b: IntArray): Int {
-        val zipAB = a.zip(b)
-        val answer: Int = zipAB.sumOf { (a, b) -> a * b }
-
-        return answer
+        return (0 until a.size).fold(0) { acc, index ->
+            acc + a[index] * b[index]
+        }
     }
+
+    /*
+    
+    // zip으로 새로운 list를 만들기 보다는 있는 걸 최대한 활용하자.
+    fun solution(a: IntArray, b: IntArray): Int {
+        return a.zip(b)
+            .map { (a, b) -> a * b }
+            .sum()
+    }
+
+    */
 }
