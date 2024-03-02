1c016efb87892506a401be8d1429cd9d8c2a2167
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-02 16:56:57 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 21.42 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
@@ -1,8 +1,3 @@
 class Solution {
-    fun solution(numbers: IntArray): Int {
-        var numberRange = Array(9) { it + 1 } // 0..8 -> 1..9
-        
-        return numberRange.filter { it !in numbers }
-            .sumOf { it }
-    }
+    fun solution(numbers: IntArray) = Array(9) { it + 1 }.filter { it !in numbers }.sum()
 }
