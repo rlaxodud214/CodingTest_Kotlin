dceb4c4deaf5d83a132717622e305b4309c581e8
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-02 21:01:11 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.07 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
@@ -1,5 +1,6 @@
 class Solution {
     fun solution(price: Int, money: Int, count: Int): Long {
+        // 총 이용 횟수 구할 때, sum()함수 사용 시 소요 시간 8ms 정도 추가됨 - (1..count).sum()
         val totalCount = count * (count + 1) / 2  // 합 공식 : n * (n+1) / 2
         val amountPrice = price.toLong() * totalCount
 
