7e6e249044ce3a85985549cfab218e45368c4469
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-10 22:39:54 +0900

    [level 1] Title: 카드 뭉치, Time: 0.29 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
@@ -1,17 +1,16 @@
 class Solution {
-    fun solution(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
-        var index1 = 0
-        var index2 = 0
-        
-        goal.forEach { 
-            if (it != cards1[index1] && it != cards2[index2]) 
-                return "No"
-            
-            if (index1 < cards1.size - 1 && it == cards1[index1])
-                index1++
-            if (index2 < cards2.size - 1 && it == cards2[index2])
-                index2++
+    fun solution(cards1: Array<String>, cards2: Array<String>, goals: Array<String>): String {
+        var answer = true
+        var (index1, index2) = Pair(0, 0)
+
+        for(goal in goals) {
+            when {
+                index1 < cards1.size && goal == cards1[index1] -> index1++
+                index2 < cards2.size && goal == cards2[index2] -> index2++
+                else -> answer = false
+            }
         }
-        return "Yes"
+
+        return if(answer) "Yes" else "No"
     }
 }
