54df99b8c401d7e16d1b68bea3ff52f81ed9db7f
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-10 22:43:02 +0900

    [level 1] Title: 카드 뭉치, Time: 0.59 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
@@ -1,16 +1,15 @@
 class Solution {
     fun solution(cards1: Array<String>, cards2: Array<String>, goals: Array<String>): String {
-        var answer = true
         var (index1, index2) = Pair(0, 0)
 
         for(goal in goals) {
             when {
                 index1 < cards1.size && goal == cards1[index1] -> index1++
                 index2 < cards2.size && goal == cards2[index2] -> index2++
-                else -> answer = false
+                else -> return "No" // Answer이 false인 상태에서 무의미하게 추가로 도는 로직 개선
             }
         }
 
-        return if(answer) "Yes" else "No"
+        return "Yes"
     }
 }
