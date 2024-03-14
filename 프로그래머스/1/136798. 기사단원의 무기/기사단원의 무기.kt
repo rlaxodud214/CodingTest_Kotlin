51415102e8f80617edbb6a37f27acf9ced0925ec
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-14 13:57:19 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.04 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.kt
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.kt
@@ -1,28 +1,21 @@
-import kotlin.math.sqrt
-
 class Solution {
+    // 다른 사람의 아이디어를 이해한 뒤, 재구현 했습니다.
+    val measureCount = IntArray(100_000) { 0 }
+    
+    init {
+        // i가 2일때, 모든 2의 배수에 + 1 (약수의 개수 + 1)
+        for (i in 0..measureCount.size) {
+            for (j in i until measureCount.size step(i + 1)) {
+                measureCount[j]++
+            }
+        }
+    }
+    
     fun solution(number: Int, limit: Int, power: Int): Int {
         var answer = 0
 
-        // 1. 1부터 number까지 약수의 개수를 구한다.
         for (i in 0 until number) {
-            val square = sqrt((i+1).toDouble())
-            var count = 0
-
-            for (j in 1..square.toInt()) { // 제곱근 까지 돌고 * 2
-                if ((i+1) % j == 0) {
-                    count += 2
-                }
-            }
-
-            // num이 16인 경우, 4가 2번 카운트 되었으므로 -- 해줘야한다.
-            // square가 정수인지 판단해야함 -> 소수점이 0인지 체크
-            if (square - square.toInt() == 0.0) { //
-                count--
-            }
-
-            // 2. limit를 넘으면 power를 아니면 count를 누적한다.
-            answer += if(count > limit) power else count
+            answer += if (measureCount[i] > limit) power else measureCount[i]
         }
 
         return answer
