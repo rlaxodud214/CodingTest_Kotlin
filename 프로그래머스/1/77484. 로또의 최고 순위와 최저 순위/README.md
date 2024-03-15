aae09bed36d6965612b9171907fca7d2372bad8e
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-15 13:32:28 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
 
 ### 성능 요약
 
-메모리: 62.1 MB, 시간: 0.01 ms
+메모리: 61.5 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 6일 18:12:17
+2024년 03월 15일 13:32:24
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
@@ -1,23 +1,23 @@
 class Solution {
     fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
-        var matchCount = 0
-        
+        var matchCount = IntArray(2) { 7 } // max, min
+
         // 1. lottos와 win_nums 간에 일치하는 숫자의 개수를 파악한다.
-        lottos.forEach { lotto ->
-            matchCount += win_nums.count { it == lotto }
+        lottos.forEach { lottoNumber ->
+            matchCount[0] -= win_nums.count { it == lottoNumber }
+            matchCount[1] -= win_nums.count { it == lottoNumber }
         }
-        
-        // 2. lottos에 있는 0의 개수를 파악한다.
-        var zeroCount = lottos.count { it == 0}
-        
-        // 3. 최소 순위 = 7 - 당첨 숫자 개수
-        var max = 7 - matchCount - zeroCount
-        var min = 7 - matchCount
-        
-        // map 썼을 때, 5.89ms -> if문 썼을 때, 0.02ms 시간차이 많이남
-        if(max > 6) max = 6
-        if(min > 6) min = 6
-        
-        return intArrayOf(max, min)
+
+        // 2. lottos의 0의 개수를 빼준다.
+        matchCount[0] -= lottos.count { it == 0 }
+
+        // 3. 낙첨일 경우 6등으로 변환한다.
+        for (i in matchCount.indices) {
+            if (matchCount[i] == 7) {
+                matchCount[i] = 6
+            }
+        }
+
+        return matchCount
     }
 }
