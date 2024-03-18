f4cafe9d4378aef193248fca4cb8a98a0d3022a8
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-18 12:12:39 +0900

    [level 1] Title: 숫자 짝꿍, Time: 12.29 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
@@ -1,31 +1,14 @@
 class Solution {
     fun solution(X: String, Y: String): String {
-        val numberCountX = MutableList(10) { 0 } // 0 ~ 9
-        val numberCountY = MutableList(10) { 0 } // 0 ~ 9
         val commonNumberCount = MutableList(10) { 0 } // 0 ~ 9
 
-
         // 1. 공통으로 포함된 수를 뽑는다.
-        for (chx in X) {
-            for (chy in Y) {
-                if (chx == chy) {
-                    numberCountX[chx - '0']++
-                    break
-                }
-            }
-        }
-
-        for (chy in Y) {
-            for (chx in X) {
-                if (chy == chx) {
-                    numberCountY[chy - '0']++
-                    break
-                }
-            }
-        }
+        val xCountMap = X.groupingBy { it }.eachCount()
+        val yCountMap = Y.groupingBy { it }.eachCount()
 
         for (i in commonNumberCount.indices) {
-            commonNumberCount[i] = minOf(numberCountX[i], numberCountY[i])
+            val convertIndex = (48 + i).toChar()
+            commonNumberCount[i] = minOf(xCountMap[convertIndex] ?: 0, yCountMap[convertIndex] ?: 0)
         }
 
         if (commonNumberCount.all { it == 0 }) {
@@ -33,18 +16,15 @@ class Solution {
         }
 
         var sb = StringBuilder()
-        for ((index, value) in commonNumberCount.withIndex()) {
-            if (value != 0) {
-                repeat(value) {
-                    sb.append(index)
+        for (i in 9 downTo 0) {
+            if (commonNumberCount[i] != 0) {
+                repeat(commonNumberCount[i]) {
+                    sb.append(i)
                 }
             }
         }
 
-        // 시간이 많이 소요되는 부분 -> reversed()
-        sb = sb.reversed() as StringBuilder
         val sbText = sb.toString()
-
-        return if (sb.all { it == '0' }) "0" else sbText
+        return if (sbText.all { it == '0' }) "0" else sbText
     }
 }
