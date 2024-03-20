af89e6c50295499c01983842be7722ea30afcd9d
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-20 10:00:42 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.99 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.kt
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.kt
@@ -1,37 +1,25 @@
 class Solution {
-    fun solution(keymap: Array<String>, targets: Array<String>): IntArray {
-        var answer = mutableListOf<Int>()
-        var map = mutableMapOf<Char, Int>()
-        
-        // 1. keymap을 이용해서 <"키패드" : 최소 누르는 횟수> 형태의 map 생성
-        keymap.forEach { str ->
-            // 키가 이미 있는 경우 최소값 유지하기
-            str.forEachIndexed { index, ch ->
-                if (!map.containsKey(ch)) {
-                    map[ch] = index + 1
+    fun solution(keymaps: Array<String>, targets: Array<String>): IntArray {
+        var answer = IntArray(targets.size) { -1 }
+        var matchIndex = mutableMapOf<Char, Int>()
+
+        for (keymap in keymaps) { // ABACD
+            for(i in keymap.indices) {
+                if (!matchIndex.containsKey(keymap[i])) {
+                    matchIndex[keymap[i]] = i + 1
+                } else if (matchIndex[keymap[i]]!! > i + 1){
+                    matchIndex[keymap[i]] = i + 1
                 }
-                else if (map[ch]!! > index + 1) {
-                    map[ch] = index + 1
-                }    
-                // [다른 사람 코드] 위 if, else if와 같은 코드 
-                // minOf(기존 else if), "?:"(기존 if) 연산자를 사용함
-                // map[ch] = minOf(map[ch] ?: 100, i + 1) 
             }
         }
-        
-        // 2. map을 이용해서 targets을 완성하는 최소값 합산하기
-        // 다른 사람의 풀이 -> 아래처럼 "outer@"를 명시적으로 지칭할 수 있음!! + 이름 수정 가능함
-        targets.forEach outer@{
-            var ac = it.fold(0) { acc, ch -> 
-                // ch가 map의 키값에 없다면 -1 추가하기
-                if (!map.containsKey(ch)) {
-                    answer.add(-1)
-                    return@outer
-                }
-                acc + map[ch]!!
+
+        for (i in targets.indices) { // ABCD
+            if (targets[i].any { matchIndex.containsKey(it).not() }) {
+                answer[i] = -1
+            } else {
+                answer[i] = targets[i].map { matchIndex[it]!! }.sumOf { it }
             }
-            answer.add(ac)
         }
-        return answer.toIntArray()
+        return answer
     }
 }
