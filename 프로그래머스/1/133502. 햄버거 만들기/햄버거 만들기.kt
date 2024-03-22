4b2adc725fcb9b1cd3fad98aae0303707943f95a
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-22 10:39:16 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.17 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.kt
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.kt
@@ -1,19 +1,31 @@
+import java.util.Stack
+
 class Solution {
     fun solution(ingredient: IntArray): Int {
-        var ingredientCopy = ingredient.toMutableList()
         var answer: Int = 0
-        var idx = -1
-        
-        while(idx < ingredientCopy.size - 4) {
-            idx++
-            if(ingredientCopy[idx] == 1 && ingredientCopy[idx+1] == 2 && ingredientCopy[idx+2] == 3 && ingredientCopy[idx+3] == 1) {
-                
-                for(i in 1..4) ingredientCopy.removeAt(idx)
+        var stack = Stack<Int>()
 
-                if(idx > 3) idx-= 3 else idx = -1
+        for (num in ingredient) {
+            stack.push(num)
+
+            if (stack.size < 4 || num != 1) {
+                continue
+            }
+
+            var sb = StringBuilder()
+
+            for (i in 1..4) {
+                sb.append(stack[stack.size - i])
+            }
+
+            if (sb.toString() == "1321") {
+                repeat(4) {
+                    stack.pop()
+                }
                 answer++
             }
         }
+
         return answer
     }
 }
