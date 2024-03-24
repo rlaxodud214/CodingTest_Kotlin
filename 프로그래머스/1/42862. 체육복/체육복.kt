03d53b34e6838756260e799971cef1034c1165b4
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-25 02:42:33 +0900

    [level 1] Title: 체육복, Time: 13.83 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.kt
+++ 프로그래머스/1/42862. 체육복/체육복.kt
@@ -1,42 +1,33 @@
 class Solution {
-    // 그리디
     fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
         var answer = n
-        
-        // 1. 매개변수 3개를 이용 -> 학생이 보유한 체육복의 수를 값으로 하는 배열 만들기
-        var storageA = IntArray(n + 1) { 1 } // n + 1개 생성, 0번은 빈자리로 채워서 No 맞추기
-        reserve.forEach { i -> storageA[i] = 2 }
-        lost.forEach { i -> storageA[i] -= 1 } // lost가 도난 당한 경우 포함
-        var storageB = storageA.copyOf() // CaseB를 위함
-        
-        // 2. reserve를 순회하며 좌-우에 lost가 있는지 파악하고 체육복 전달하기
-        reserve.forEach { i -> 
-            if(storageA[i] != 2)
-                return@forEach // continue
-            
-            // A. 앞번호 학생에게 먼저 주는 경우
-            if (storageA[i-1] == 0) {
-                storageA[i-1] = 1
-                storageA[i] = 1
-            }
-            else if (i < n && storageA[i+1] == 0) {
-                storageA[i+1] = 1
-                storageA[i] = 1
-            }
-            
-            // B. 뒷번호 학생에게 먼저 주는 경우
-            if (i < n && storageB[i+1] == 0) {
-                storageB[i+1] = 1
-                storageB[i] = 1
-            }
-            else if (storageB[i-1] == 0) {
-                storageB[i-1] = 1
-                storageB[i] = 1
-            }
+        var clothesCount = IntArray(n + 1) { 1 }
+
+        for (i in lost) {
+            clothesCount[i]--
         }
-        
-        // 3. 체육 수업을 들을 수 있는 학생의 수 = n(전체 학생의 수) - 체육복의 수가 여전히 0인 학생의 수
-        var zeroCount = minOf(lost.count{ i -> storageA[i] == 0 }, lost.count{ i -> storageB[i] == 0 })
-        return answer - zeroCount
+        for (i in reserve) {
+            clothesCount[i]++
+        }
+
+        for (idx in lost.sorted()) {
+            if (clothesCount[idx] != 0) {
+                continue
+            }
+
+            if (idx - 1 >= 0 && clothesCount[idx - 1] == 2) {
+                clothesCount[idx - 1]--
+                clothesCount[idx]++
+                continue
+            }
+
+            if (idx + 1 < clothesCount.size && clothesCount[idx + 1] == 2) {
+                clothesCount[idx + 1]--
+                clothesCount[idx]++
+                continue
+            }
+            answer--
+        }
+        return answer
     }
 }
