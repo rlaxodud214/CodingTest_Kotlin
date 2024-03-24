d7e67ce917b4d43c5d19c018924744695e020e87
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-25 03:22:31 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 20.87 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
@@ -1,24 +1,30 @@
 class Solution {
     fun solution(survey: Array<String>, choices: IntArray): String {
-        var answer: String = ""
-        // 1. survey_result를 저장할 맵 선언 / choices
-        var survey_R = mutableMapOf<String, Int>("RT" to 0, "CF" to 0, "JM" to 0, "AN" to 0)
-        
-        // 2. choices -> score : 1 -> 3, 4 -> 0, 5 -> -1 / score == 4 - choices
-        for((index, type) in survey.withIndex()) {
-            if(choices[index] == 4) continue
-            
-            if(survey_R.containsKey(type)) {
-                survey_R[type] = survey_R[type]!! + (4 - choices[index])
+        var result_info = listOf("RT", "CF", "JM", "AN")
+        var result = IntArray(4) { 0 }
+
+        for ((index, value) in survey.withIndex()) {
+            if (choices[index] == 4) {
+                continue
+            }
+
+            var typeNumber = result_info.indexOf(value)
+
+            if (typeNumber != -1) {
+                result[typeNumber] += (4 - choices[index])
             } else {
-                survey_R[type.reversed()] = survey_R[type.reversed()]!! - (4 - choices[index])
+                typeNumber = result_info.indexOf(value.reversed())
+                result[typeNumber] -= (4 - choices[index])
             }
         }
 
-        // 3. score >= 0이면 앞의 유형 else 뒤의 유형
-        for ((key, score) in survey_R) {
-            answer += if(score >= 0) key[0] else key[1]
+        var sb = StringBuilder()
+
+        for (i in result.indices) {
+            val chooseCharIndex = if (result[i] >= 0) 0 else 1 
+            sb.append(result_info[i][chooseCharIndex])
         }
-        return answer
+
+        return sb.toString()
     }
 }
