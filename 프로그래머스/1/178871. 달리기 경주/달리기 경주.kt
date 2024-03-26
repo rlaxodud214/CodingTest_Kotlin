02dd6be1a1618bf091d4e7264fca26067a00feed
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-26 09:34:57 +0900

    [level 1] Title: 달리기 경주, Time: 0.22 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.kt
@@ -0,0 +1,33 @@
+class Solution {
+    fun solution(players: Array<String>, callings: Array<String>): Array<String> {
+        var answer: Array<String> = arrayOf<String>()
+        
+        // 1) indexOf의 시간복잡도 이슈로 인해서 시간 초과
+        // for(target in callings) {
+        //     val curIndex = players.indexOf(target)
+        //     val preIndex = players.indexOf(target)-1
+        //     players[curIndex] = players[preIndex].also { players[preIndex] = players[curIndex] } 
+        // }
+        
+        // 2) Map을 통해 인덱스 백업 <이름, index>
+        val playerMaps = mutableMapOf<String, Int>()
+        for ((index, player) in players.withIndex()) {
+            playerMaps[player] = index
+        }
+        
+        for(target in callings) {
+            val curIndex = playerMaps[target] ?: null;
+            val preIndex = curIndex?.minus(1) ?: null;
+            
+            if (curIndex == null || preIndex == null) break
+            
+            // 순위 변경
+            players[curIndex] = players[preIndex].also { players[preIndex] = players[curIndex] } 
+                
+            // 순위 변경에 따라 playerMaps 최신화
+            playerMaps[players[curIndex]] = curIndex
+            playerMaps[players[preIndex]] = preIndex
+        }
+        return players
+    }
+}
