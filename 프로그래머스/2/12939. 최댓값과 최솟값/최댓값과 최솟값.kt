07c71b6a5878b12609a9f2c01665276d797a2c97
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-03-29 11:40:52 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 21.20 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
@@ -4,19 +4,6 @@ class Solution {
             .map { it.toInt() }
 
         // min, max -> unresolved reference 오류남
-        // return "${answer.min()} ${answer.max()}"
-
-        var (min, max) = Pair(Int.MAX_VALUE, Int.MIN_VALUE)
-
-        for (num in answer) {
-            if (num < min) {
-                min = num
-            }
-            if (num > max) {
-                max = num
-            }
-        }
-
-        return "${min} ${max}"
+        return "${answer.minOf{ it }} ${answer.maxOf{ it }}"
     }
 }
