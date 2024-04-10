871cc5629557a9f0d800c23594fc29bbadf649c6
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-04-11 00:33:53 +0900

    [level 2] Title: 괄호 회전하기, Time: 10.05 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.kt
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.kt
@@ -1,48 +1,54 @@
+import java.util.Stack
+
 class Solution {
     fun solution(s: String): Int {
         var answer: Int = 0
+        val texts = Array(s.length) { s }
         
-        // 1. 문자열 회전
-        var sCopy = s
-        var stack = mutableListOf<Char>()
-        
-        outer@ for (i in 0 until s.length) {
-        	// 2. 스택을 통해 올바른 괄호인지 판단
-            sCopy = sCopy.substring(1) + sCopy[0]
-            
-            for (ch in sCopy) {
-                // 앞에 여는 괄호 없이 닫는 괄호가 온 경우
-                if ( stack.size == 0 && ch in ")]}") {
-                    continue@outer
-                }
-                
-                when {
-                    ch in "([{" -> stack.add(ch)
-                    
-                    ch in ")]}" -> {
-                        // 페어가 불일치한 경우
-                    	if (ch != pairs[stack.last()]) {
-                            continue@outer
-                        }
-                        // 페어가 일치한 경우
-                        stack.removeAt(stack.size - 1)
-                    }
-                }
-            }
-            
-            if (stack.size == 0) {
+        // 1. 괄호 회전하기
+        for (i in 0 until texts.size) {
+            var sb = StringBuilder()
+            sb.append(s.drop(i))
+            sb.append(s.dropLast(texts.size - i))
+            texts[i] = sb.toString()
+        }
+
+        // 2. 올바른 괄호인지 판단하며, count++
+        for (text in texts) {
+            val result = isCollect(text)
+            if (result) {
                 answer++
             }
         }
-        
+
         return answer
     }
-    
+
+    fun isCollect(text: String): Boolean {
+        val stack = Stack<Char>()
+
+        for (ch in text) {
+            when(ch) {
+                '(', '{', '[' -> stack.add(ch)
+                ')', '}', ']' -> {
+                    if (stack.isEmpty()) {
+                        return false
+                    }
+                    if (stack.pop() != pairs[ch]) {
+                        return false
+                    }
+                }
+            }
+        }
+
+        return stack.isEmpty()
+    }
+
     companion object {
         var pairs = mapOf(
-            '(' to ')',
-            '{' to '}',
-            '[' to ']',
+            ')' to '(',
+            '}' to '{',
+            ']' to '[',
         )
     }
 }
