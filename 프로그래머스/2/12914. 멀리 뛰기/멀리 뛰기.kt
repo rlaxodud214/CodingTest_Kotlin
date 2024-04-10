e7f3c244c897f8fe2985ac38183856a30a86ab4c
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-04-10 22:02:53 +0900

    [level 2] Title: 멀리 뛰기, Time: 2.93 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.kt
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.kt
@@ -1,14 +1,16 @@
 class Solution {
     fun solution(n: Int): Long {
-        return when(n) {
-            1 -> 1L
-            2 -> 2L
-            else -> {
-                (3..n).fold(Pair(1L, 2L)) { acc, _ ->
-                    val (first, second) = acc
-                    Pair(second, (first + second) % 1234567)
-                }.second
-            }
+        if (n <= 2) {
+            return n.toLong()
         }
+
+        // n == 5일 때 -> 4에서 1칸 가기 + 3에서 2칸 가기
+        // f(n) = f(n-1) + f(n-2)
+        return (3..n).fold(Pair(1L, 2L)) { acc, num ->
+            val fn = acc.run {
+                (first + second) % 1234567
+            }
+            Pair(acc.second, fn)
+        }.second
     }
 }
