62e075b5f68c68b6610e6c99a65472803e643a19
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-04-10 23:35:04 +0900

    [level 2] Title: 귤 고르기, Time: 67.38 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class Solution {
     /**
      * 한 상자에 담으려는 귤의 개수 k    1 ≤ k ≤ tangerine의 길이 ≤ 100,000
@@ -6,21 +8,28 @@ class Solution {
     fun solution(k: Int, tangerine: IntArray): Int {
         // 1. tangerine에서 각 요소의 개수를 뽑는다. -> 내림차순으로 정렬 -> 시간 오래 걸릴 듯
         val numCounts = IntArray(10_000_001) { 0 }
-        
+
         for (i in tangerine) {
-            numCounts[i]++    
+            numCounts[i]++
         }
-        numCounts.sortDescending()
-        
+
+        val pq = PriorityQueue<Int>(Collections.reverseOrder())
+        numCounts.forEach {
+            if (it != 0) {
+                pq.add(it)
+            }
+        }
+
         // 2. 요소의 개수를 더하다가 K보다 커지면 종료
         var count: Int = 0
+        var sum = 0
 
-        numCounts.fold(0) { acc, i ->
-            if (acc >= k) {
+        while (!pq.isEmpty()) {
+            if (sum >= k) {
                 return count
             }
             count++
-            acc + i
+            sum += pq.poll() // 항상 최대값
         }
 
         return count
